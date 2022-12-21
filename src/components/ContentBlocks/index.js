a9a60048ab9a1faeb7b07f19553f511d34e418d5
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-12-21 14:27:56 +0000

    refactor: ContentBlocks component

--- src/components/ContentBlocks/index.js
+++ src/components/ContentBlocks/index.js
@@ -2,55 +2,23 @@ import React from 'react';
 import styles from './styles.module.css';
 import ContentBox from './ContentBox';
 
-const contentBoxProps = {
-  overview: {
-    title: 'Getting Started',
-  },
-  learn: {
-    title: 'Learn',
-  },
-  govern: {
-    title: 'Govern',
-  },
-  develop: {
-    title: 'Develop',
-  },
-};
-
-export default function ContentBlocks({ image, title, text, footer }) {
+export default function ContentBlocks({ image, title, text, children }) {
   if (title === 'Learn') {
     return (
       <div>
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.text}>{text}</p>
-        <ContentBox title={contentBoxProps.learn.title} />
+        <ContentBox title="Learn" />
       </div>
     );
   }
 
-  if (title === 'Govern') {
-    return (
-      <div>
-        <div className={styles.figs}>
-          <img src={image} />
-        </div>
-        <h2 className={styles.title}>{title}</h2>
-        <p className={styles.text}>{text}</p>
-        <ContentBox title={contentBoxProps.govern.title} />
-      </div>
-    );
-  }
-
-  if (footer) {
-    return (
-      <div>
-        <div className={styles.figs}>
-          <img src={image} />
-        </div>
-        <h2 className={styles.title}>{title}</h2>
-        <p className={styles.text}>{text}</p>
-        <ContentBox title={contentBoxProps.develop.title} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      {children}
+      <h2 className={styles.title}>{title}</h2>
+      <p className={styles.text}>{text}</p>
+      <ContentBox title="Develop" />
+    </div>
+  );
 }
